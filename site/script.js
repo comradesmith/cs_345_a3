32eb8fb61574a6ae113c7f025aa7fcbc1d152483
cam <cameronlh.smith@outlook.com>
2017-05-21 18:34:43 +1200

    implement quick and dirty dynamic comments for feedback

--- site/script.js
+++ site/script.js
@@ -41,3 +41,31 @@ function toggle_section(incoming_section) {
 	});
 
 }
+
+function reply(button) {
+	var reply_area = "<div class='reply_box'><textarea rows='12' cols='60' placeholder='Enter reply...'></textarea><br><button onclick='submit_comment(this)'>Submit</submit></div>";
+	var reply_container = $(button).parent().parent();
+	
+	reply_container.children(".comment.reply_button").remove();
+	reply_container.append(reply_area);
+}
+
+function submit_comment(button){
+	var parent_element = $(button).parent();
+	var reply_container = parent_element.parent();
+	var text_area = $(button).siblings('textarea');
+	var input_text = text_area.val();
+	// parent_element.empty();
+	
+	var new_comment = "<div class='comment reply'><h4>Me</h4><p>"
+					 +  input_text + "</p></div>";
+
+	var reply_button = "<div class='comment reply_button'><a onclick='reply(this)'>Reply</a></div>";
+
+	// parent_element.remove();
+	console.log(reply_container.children(".reply_box"));	
+	reply_container.children(".reply_box").remove();
+	reply_container.append(new_comment, reply_button);
+
+	console.log(input_text);
+}
