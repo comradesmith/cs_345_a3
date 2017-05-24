cd97c05bf20ed2e3a5112c1c7ac889e09ae9d326
cam <cameronlh.smith@outlook.com>
2017-05-24 20:23:30 +1200

    move menu to top of page

--- site/script.js
+++ site/script.js
@@ -6,20 +6,12 @@ function toggle_menu(hamburger) {
 	if(menu_open){
 		menu_open = false;
 		hamburger.style.background = "#46C";
-		$("nav.toggleable #nav_options").css({
-			"visibility" : "hidden",
-			"display" : "none",
-			"z-index" : "-9001"
-		});
+		$("nav.toggleable #nav_options").hide();
 	}
 	else {
 		menu_open = true;
 		hamburger.style.background = "#C64";
-		$("nav.toggleable #nav_options").css({
-			"visibility" : "visible",
-			"display" : "block",
-			"z-index" : "1"
-		});
+		$("nav.toggleable #nav_options").show();
 	}
 } 
 
